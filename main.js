ec69ff93e60b0e468b32983f80398a6a4cc8a65b
Corina Pelichowski <corina@danitt.com>
2019-12-21 13:14:00 +1000

    code clean up in main.js

--- main.js
+++ main.js
@@ -3,101 +3,10 @@ const ctx = canvas.getContext('2d');
 const canvasNext = document.getElementById('next');
 const ctxNext = canvasNext.getContext('2d');
 
-// Calculate size of canvas from constants.
-//ctx.canvas.width = COLS * BLOCK_SIZE;
-//ctx.canvas.height = ROWS * BLOCK_SIZE;
-
-
-// Scale blocks
-
-//ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
-
-let board = new Board();
-
-function play() {
-  board.reset();
-  console.table(board.grid);
-}
-
-function play() {
-  board = getEmptyBoard();
-  let piece = new Piece(ctx);
-  piece.draw();
-  
-  board.piece = piece;
-}
-
-moves = {
-    [KEY.LEFT]: p => ({ ...p, x: p.x - 1 }),
-    [KEY.RIGHT]: p => ({ ...p, x: p.x + 1}),
-    [KEY.UP]: p => ({ ...p, y: p.y + 1})
-};
-
-document.addEventListener('keydown', event => {
-    if (moves[event.keyCode]){
-        // Stop the event from bubbling.
-        event.preventDefault();
-
-        // Get new state of piece
-        let p = moves[event.keyCode] (board.piece);
-
-        if (board.valid(p)) {
-            // If the move is valid, move the piece
-            board.piece.move(p);
-
-            // Clear old position before drawing.
-            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-            board.piece.draw();
-        }
-        
-    }
-
-const KEY = {
-    SPACE: 32,
-    // ...
-}
-
-moves = {
-    [KEY.SPACE]: p => ({ ...p, y: p.y + 1})
-    // ...
-};
-
-// In EventListener
-if (event.keyCode === KEY.SPACE) {
-    // Hard drop
-    while (board.valid(p)) {
-        board.piece.move(p);
-        p = moves[KEY.DOWN] (board.piece);
-    }
-}
-
-});
-
-function animate(now = 0) {
-    // Update elapsed time.
-    time.elapsed = now - time.start;
-
-    // If elapsed time passed time for current level
-    if (time.elapsed > time.level) {
-    
-    // Restart counting from now
-    time.start = now;
-
-    this.drop();
-    }
-
-    // Clear board before drawing new state.
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-    board.draw();
-    requestID = requestAnimationFrame(animate);    
-}
-
 let accountValues = {
     score: 0,
-    lines: 0,
-    level: 0
+    level: 0,
+    lines: 0
 }
 
 function updateAccount(key, value) {
@@ -113,26 +22,114 @@ let account = new Proxy(accountValues, {
         updateAccount(key, value);
         return true;
     }
-})
+});
 
-if (event.keyCode === KEY.SPACE) {
-    while (board.valid(p)) {
-        account.score += POINTS.HARD_DROP;
-        board.piece.move(p);
-        p= moves[KEY.DOWN] (board.piece);
+let requestId;
+
+moves = {
+    [KEY.LEFT]: p => ({ ...p, x: p.x - 1 }),
+    [KEY.RIGHT]: p => ({ ...p, x: p.x + 1 }),
+    [KEY.DOWN]: p => ({ ...p, y: p.y + 1 }),
+    [KEY.SPACE]: p => ({ ...p, y: p.y + 1 }),
+    [KEY.UP]: p => board.rotate(p)
+};
+
+let board = new Board(ctx, ctxNext);
+addEventListener();
+initNext();
+
+function initNext() {
+    // Calculate size of canvas from constants.
+    ctxNext.canvas.width = 4 * BLOCK_SIZE;
+    ctxNext.canvas.height = 4 * BLOCK_SIZE;
+    ctxNext.scale(BLOCK_SIZE, BLOCK_SIZE);
+}
+
+function addEventListener() {
+    document.addEventListener('keydown', event => {
+        if (event.keyCode === KEY.P) {
+            pause();
+        }
+        if (event.keyCode === KEY.ESC) {
+            gameOver();
+        } else if (moves[event.keyCode]) {
+            event.preventDefault();
+            // Get new state
+            let p = moves[event.keyCode](board.piece);
+            if (event.keyCode === KEY.SPACE) {
+                // Hard drop
+                while (board.valid(p)) {
+                    account.score += POINTS.HARD_DROP;
+                    board.piece.move(p);
+                    p = moves[KEY.DOWN](board.piece);
+                }
+            } else if (board.valid(p)) {
+                board.piece.move(p);
+                if (event.keyCode === KEY.DOWN) {
+                    account.score += POINTS.SOFT_DROP;
+                }
+            }
+        }
+    });
+}
+
+function resetGame() {
+    account.score = 0;
+    account.lines = 0;
+    account.level = 0;
+    board.reset();
+    time = { start: 0, elapsed: 0, level: LEVEL[account.level] };
+}
+
+function play() {
+    resetGame();
+    time.start = performance.now();
+    // If we have an old game running a game then cancel the old
+    if (requestId) {
+        cancelAnimationFrame(requestId);
     }
-} else if (board.valid(p)) {
-    board.piece.move(p);
-    if (event.keyCode === KEY.DOWN) {
-        account.score += POINTS.SOFT_DROP;
+
+    animate();
+}
+
+function animate(now = 0) {
+    time.elapsed = now - time.start;
+    if (time.elapsed > time.level) {
+        time.start = now;
+        if (!board.drop()) {
+            gameOver();
+            return;
+        }
     }
+
+    // Clear board before drawing new state.
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    board.draw();
+    requestId = requestAnimationFrame(animate);
 }
 
 function gameOver() {
-    cancelAnimationFrame(requestID);
-    this.ctx.fillStyle = 'black';
-    this.ctx.fillRect(1, 2, 3, 8, 1.2);
-    this.ctx.font = '1px Arial';
-    this.ctx.fillStyle = 'red';
-    this.ctx.fillText('GAME OVER', 1.8, 4);
+    cancelAnimationFrame(requestId);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(1, 3, 8, 1.2);
+    ctx.font = '1px Arial';
+    ctx.fillStyle = 'red';
+    ctx.fillText('GAME OVER', 1.8, 4);
+}
+
+function pause() {
+    if (!requestId) {
+        animate();
+        return;
+    }
+
+    cancelAnimationFrame(requestId);
+    requestId = null;
+
+    ctx.fillStyle = 'black';
+    ctx.fillRect(1, 3, 8, 1.2);
+    ctx.font = '1px Arial';
+    ctx.fillStyle = 'yellow';
+    ctx.fillText('PAUSED', 3, 4);
 }
